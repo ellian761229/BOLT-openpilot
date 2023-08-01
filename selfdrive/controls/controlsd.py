54c260b6e861a9180df14111a4e0e2ec3101fbad
ellian761229 <107234449+ellian761229@users.noreply.github.com>
2023-08-01 12:46:07 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -478,9 +478,12 @@ class Controls:
     #    self.v_cruise_kph = CS.cruiseState.speed * CV.MS_TO_KPH
     #  else:
     #    self.v_cruise_kph = 0
-
     if CS.adaptiveCruise:
       self.v_cruise_kph = update_v_cruise(self.v_cruise_kph, CS.buttonEvents, self.button_timers, self.enabled, self.is_metric)
+      if CS.gasPressed:
+        if CS.vEgo*3.6 > self.v_cruise_kph:
+          self.v_cruise_kph = CS.vEgo*3.6
+    
       if CS.regenPressed:
         self.v_cruise_kph = update_v_cruise_regen(CS.vEgo, self.v_cruise_kph, CS.regenPressed, self.enabled)
         self.regenPressed = True
