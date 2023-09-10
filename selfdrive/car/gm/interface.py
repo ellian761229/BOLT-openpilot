0f3f61954de4c9857f2eb43a83c145997d2d2a79
ellian761229 <107234449+ellian761229@users.noreply.github.com>
2023-09-10 19:54:19 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -188,9 +188,9 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayLowerBound = 0.13
     ret.longitudinalActuatorDelayUpperBound = 0.15
     
-    ret.stopAccel = -2.0 
+    ret.stopAccel = -2.5 
     ret.stoppingDecelRate = 4.0 #속도감속힘
-    ret.vEgoStopping = 0.5
+    ret.vEgoStopping = 0.4
     ret.vEgoStarting = 0.5
     ret.stoppingControl = True
     
