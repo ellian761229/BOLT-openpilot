22f1ac238d0da1c8e99fd17aff7edaf5aa5f438d
ellian761229 <107234449+ellian761229@users.noreply.github.com>
2023-09-26 15:01:01 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -60,7 +60,7 @@ T_IDXS_LST = [index_function(idx, max_val=MAX_T, max_idx=N) for idx in range(N+1
 
 T_IDXS = np.array(T_IDXS_LST)
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
-MIN_ACCEL = -3.6
+MIN_ACCEL = -3.5
 T_FOLLOW = 1.45
 COMFORT_BRAKE = 2.3
 STOP_DISTANCE = 5.7 #정지시 앞차거리
