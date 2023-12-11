4e858cd5572b6263a388bda79f94e20c6cd935a8
ellian761229 <107234449+ellian761229@users.noreply.github.com>
2023-12-11 17:21:49 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -35,7 +35,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     # return params.ACCEL_MIN, params.ACCEL_MAX
     accel_max_bp = [10., 20., 30., 40., 50., 60., 70., 80.]
-    accel_max_v = [0.86, 0.93, 0.96, 0.99, 1.0, 1.05, 1.1, 1.15]
+    accel_max_v = [0.87, 0.93, 0.96, 0.99, 1.0, 1.05, 1.1, 1.15]
 
     return params.ACCEL_MIN, interp(v_current_kph, accel_max_bp, accel_max_v)
 
@@ -181,7 +181,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kpBP = [0., 17.9 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kpV = [1.10, 1.0, 0.76]
     ret.longitudinalTuning.kiBP = [0., 18.2 * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.180, 0.125, 0.12]  # 앞차 거리
+    ret.longitudinalTuning.kiV = [0.180, 0.125, 0.115]  # 앞차 거리
     
     ret.longitudinalTuning.deadzoneBP = [0., 30.*CV.KPH_TO_MS]
     ret.longitudinalTuning.deadzoneV = [0., 0.10]
@@ -191,7 +191,7 @@ class CarInterface(CarInterfaceBase):
     ret.stopAccel =  -0.0
     ret.stoppingDecelRate = 4.0
     ret.vEgoStopping = 0.4
-    ret.vEgoStarting = 0.4
+    ret.vEgoStarting = 0.23
     ret.stoppingControl = True
     
     ret.steerLimitTimer = 0.4
