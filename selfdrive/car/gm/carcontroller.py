d1677f6d149914c1ae1439bb5ee7d3b56d00f934
ellian761229 <107234449+ellian761229@users.noreply.github.com>
2023-12-26 18:34:24 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -112,7 +112,7 @@ class CarController():
       # self.pedal_hyst_gap = interp(CS.out.vEgo, [40.0 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS], [0.01, 0.006])
       # self.pedal_final, self.pedal_steady = actuator_hystereses(self.comma_pedal_original, self.pedal_steady, self.pedal_hyst_gap)
       # self.comma_pedal = clip(self.pedal_final, 0., 1.)
-      self.pedal_gas_max = interp(CS.out.vEgo, [0.0, 1, 2, 5, 10, 30, 50, 70], [0.2000, 0.2050, 0.2100, 0.2200, 0.2300, 0.2500, 0.2500, 0.2500])
+      self.pedal_gas_max = interp(CS.out.vEgo, [0.0, 1, 2, 5, 10, 30, 50, 70], [0.1950, 0.2000, 0.2050, 0.2150, 0.2200, 0.2400, 0.2400, 0.2400])
 
       accGain = 0.1429
       accGain3 = interp(actuators.accel, [-3.5, 2], [0.185, 0.130])
